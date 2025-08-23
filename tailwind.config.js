8ef111a570b8b7e77e838a26fbe0c49a396e607b
sh-hridoy001 <sazzadhossainhridoy961@gmail.com>
2025-08-23 23:50:55 +0600

    added file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 export default {
    content: ['*.{html,js}'],
    theme: {
-     container:{},
+     container: {
+      padding: {
+        DEFAULT: '15px',
+      },
+     },
+     screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '960px',
+      xl: '1200px',
+     },
+     fontFamily: {
+      primary: 'DM Serif Display',
+      secondary: 'Jost',
+     },
+     backgroundImage: {
+      hero: 'url(/assets/hero/bg.jpg)',
+      grid: 'url(/assets/grid.png)',
+     },
      extend: {},
    },
    plugins: [],
